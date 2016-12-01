4f27cfd69f5d07e1bb3fe29edb3285ca65374b01
Lewis Jackson <lewis.jackson@asos.com>
2016-12-01 12:10:51 +0000

    Initial commit

---
+++ lib/logstash/outputs/azurestorage.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+require "logstash/outputs/base"
+require "logstash/namespace"
+
+# An azurestorage output that does nothing.
+class LogStash::Outputs::Azurestorage < LogStash::Outputs::Base
+  config_name "azurestorage"
+
+  public
+  def register
+  end # def register
+
+  public
+  def receive(event)
+    puts event
+  end # def event
+end # class LogStash::Outputs::Azurestorage
