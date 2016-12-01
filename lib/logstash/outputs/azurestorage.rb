e85c872c80e824a1298ffedf9f783e7160336b98
Lewis Jackson <lewis.jackson@asos.com>
2016-12-01 14:59:30 +0000

    Add ability to specify parameters via logstash configuration file, create table on startup

--- lib/logstash/outputs/azurestorage.rb
+++ lib/logstash/outputs/azurestorage.rb
@@ -3,16 +3,23 @@ require "logstash/outputs/base"
 require "logstash/namespace"
 require "azure/storage"
 
-# An azurestorage output that does nothing.
 class LogStash::Outputs::Azurestorage < LogStash::Outputs::Base
   config_name "azurestorage"
 
+  config :storage_account_name, :validate => :string
+  config :storage_access_key, :validate => :string
+  config :table_name, :validate => :string
+
   public
   def register
-  end # def register
+    client = Azure::Storage::Client.create(:storage_account_name => @storage_account_name, :storage_access_key => @storage_access_key)
+    client.ca_file = File.join(File.dirname(__FILE__), "../../../assets/cacert.pem")
+    tables = client.table_client
+    tables.create_table(@table_name)
+  end
 
   public
   def receive(event)
     puts event
-  end # def event
-end # class LogStash::Outputs::Azurestorage
+  end
+end
